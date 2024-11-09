0432e783441081a3684dc52047eae2d262a4bb12
Afira <afiratahmali@gmail.com>
2024-11-09 12:35:51 +0800

    completed the counter and partially completed shopping list

---
+++ counter-starter/script.js
@@ -0,0 +1,20 @@
+const add = (step) => {
+    // We gave the number element an id of number so we can use that to get the element
+    let numberElement = document.getElementById("number");
+    // We can use the innerText property to get the text inside the element
+    let number = numberElement.innerText;
+    number = parseInt(number) + step;
+    numberElement.innerText = number;
+};
+
+const subtract = (step) => {
+    let numberElement = document.getElementById("number");
+    let number = numberElement.innerText;
+    number = parseInt(number) - step;
+    numberElement.innerText = number;
+};
+
+const reset = () => {
+    let numberElement = document.getElementById("number");
+    numberElement.innerText = 0;
+};
