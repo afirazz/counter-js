68e5f3b36719e261af2fe7565e8e9a6f3ee03bd8
Afira <afiratahmali@gmail.com>
2024-11-14 20:41:01 +0800

    started to do list

--- todo-list-starter/script.js
+++ todo-list-starter/script.js
@@ -13,3 +13,20 @@ const createNewTodoItemElement = (task, index) => {
 const toggleComplete = (index) => {
     
 };
+
+let todoTasks = ["Walk Chilli", "Make Dinner"];
+
+const todoList = document.getElementById("todo-list");
+
+for (const task of todoTasks) {
+  // Create a <p> element to store the task description
+  const newTodoTaskTextElement = document.createElement("p");
+  newTodoTaskTextElement.innerText = task;
+  
+  // Create a <li> element to contain the paragraph
+  const newTodoTaskElement = document.createElement("li");
+  newTodoTaskElement.appendChild(newTodoTaskTextElement);
+  
+  // Add the <li> element to the list
+  todoList.appendChild(newTodoTaskElement);
+}
