3b169db5265581c7ea74f6b5672c4fe89b5ae5af
Afira <afiratahmali@gmail.com>
2024-11-16 13:23:12 +0800

    created basic to do list in javascript

--- todo-list-starter/script.js
+++ todo-list-starter/script.js
@@ -1,32 +1,61 @@
-const addTask = () => {
+let todoTasks = [];
+let todoTasksStatus = [];
 
+const addTask = () => {
+  const newTask = document.getElementById("new-task-text");
+  if (newTask.value) {
+    todoTasks.push(newTask.value);
+    todoTasksStatus.push(false);
+    newTask.value = "";
+    updateTodoList();
+  }
 };
 
 const updateTodoList = () => {
+  const todoList = document.getElementById("todo-list");
+  todoList.innerHTML = "";
 
+  for (const [index, task] of todoTasks.entries()) {
+    const newTodoTaskElement = createNewTodoItemElement(task, index);
+    // Add the <li> element to the list
+    todoList.appendChild(newTodoTaskElement);
+  }
 };
 
 const createNewTodoItemElement = (task, index) => {
-
-};
-
-const toggleComplete = (index) => {
-    
-};
-
-let todoTasks = ["Walk Chilli", "Make Dinner"];
-
-const todoList = document.getElementById("todo-list");
-
-for (const task of todoTasks) {
   // Create a <p> element to store the task description
   const newTodoTaskTextElement = document.createElement("p");
   newTodoTaskTextElement.innerText = task;
-  
+
+  // Apply a CSS class to the completed items
+  if (todoTasksStatus[index] == true) {
+    newTodoTaskTextElement.classList.add("complete");
+  }  
+
   // Create a <li> element to contain the paragraph
   const newTodoTaskElement = document.createElement("li");
   newTodoTaskElement.appendChild(newTodoTaskTextElement);
   
-  // Add the <li> element to the list
-  todoList.appendChild(newTodoTaskElement);
-}
+  // Adding a button to mark each item as complete
+  const completeButtonElement = document.createElement("input");
+  completeButtonElement.type = "button";
+  completeButtonElement.value = "Completed";
+  completeButtonElement.onclick = function () {
+    toggleComplete(index);
+  };
+  newTodoTaskElement.appendChild(completeButtonElement);
+  return newTodoTaskElement
+};
+
+const toggleComplete = (index) => {
+  // If it is complete, set it to incomplete. 
+  // If it is incomplete, set it to complete.
+  if (todoTasksStatus[index] == false) {
+    todoTasksStatus[index] = true;
+  } else {
+    todoTasksStatus[index] = false;
+  }
+  updateTodoList();    
+};
+
+updateTodoList();
